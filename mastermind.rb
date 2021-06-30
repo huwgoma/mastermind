f36f1699bd6ad1b7b3030d3916e079b7ae58945b
Hugo <huwgoma@gmail.com>
2021-06-30 00:20:07 -0400

    Cleaning up

--- mastermind.rb
+++ mastermind.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-require 'pry'
 require 'colorize'
 
 # Module - Namespace for the game rules
@@ -11,7 +10,7 @@ module Rules
   end
 
   def how_to_play
-    puts "\n#{"How to play Mastermind".bold}\n\n"
+    puts "\n#{'How to play Mastermind'.bold}\n\n"
     puts 'This is a 1-player game played against the computer.'
     puts "You can choose to either be the #{'Code Maker'.bold} or the #{'Code Breaker'.bold}."
     puts 'There are 6 different number options:'
@@ -22,11 +21,11 @@ module Rules
   end
 
   def how_to_play_clues
-    puts "\n#{"Clues".bold}\n\n"
+    puts "\n#{'Clues'.bold}\n\n"
     puts 'After every guess, the Code Maker will provide you with up to 4 clues.'
     display_clue_options
-    puts "\n#{"Clue Example".bold}\n\n"
-    puts "For example: Guessing '1463' will produce 3 clues:"
+    puts "\n#{'Clue Example'.bold}\n\n"
+    puts "For example: Guessing '1463' will produce 3 clues: "
     display_code_example(1, 4, 6, 3)
     display_clue_example([1, 4, 6, 3], [1, 3, 4, 1])
   end
@@ -39,20 +38,22 @@ module Rules
 
   def display_number_options
     puts "\n"
-    Board::NUMBER_OPTIONS_DISPLAY.each { |number, number_display| print number_display + ' ' }
+    Board::NUMBER_OPTIONS_DISPLAY.each { |_number, number_display| print "#{number_display} " }
     puts "\n\n"
   end
   
-  def display_code_example(number_1, number_2, number_3, number_4)
+  def display_code_example(number_one, number_two, number_three, number_four)
     puts "\n"
-    [number_1, number_2, number_3, number_4].each { |number| print Board::NUMBER_OPTIONS_DISPLAY[number] + ' ' }
+    [number_one, number_two, number_three, number_four].each do |number|
+      print "#{Board::NUMBER_OPTIONS_DISPLAY[number]} " 
+    end
   end
 
   def display_clue_options
     puts "\n"
-    print Board::CLUE_OPTIONS_DISPLAY[:correct_number_correct_position] + "  "
+    print "#{Board::CLUE_OPTIONS_DISPLAY[:correct_number_correct_position]} "
     puts "A #{'red dot'.colorize(:red)} indicates that your guess contains 1 correct number in the correct position."
-    print Board::CLUE_OPTIONS_DISPLAY[:correct_number_wrong_position] + "  "
+    print "#{Board::CLUE_OPTIONS_DISPLAY[:correct_number_wrong_position]} "
     puts "An empty dot indicates that your guess contains 1 correct number, but in the wrong position.\n"
   end
 
@@ -65,48 +66,55 @@ end
 # Module - Namespace for the in-game text
 module GameText
   def choose_role_prompt
-    "Would you like to play as the Code Maker (1) or the Code Breaker (2)?"
+    'Would you like to play as the Code Maker (1) or the Code Breaker (2)?'
   end
 
-  def invalid_role_number_warning 
-    "Enter 1 to play as the Code Maker, or 2 to play as the Code Breaker:".colorize(:red).bold
+  def invalid_role_number_warning
+    'Enter 1 to play as the Code Maker, or 2 to play as the Code Breaker:'.colorize(:red).bold
   end
 
   def user_code_prompt
-    "Please enter your 4-digit code: "
+    'Please enter your 4-digit code: '
   end
 
   def invalid_user_code_warning
-    "Your code must be 4-digit number using numbers between 1-6:".colorize(:red).bold
+    'Your code must be 4-digit number using numbers between 1-6:'.colorize(:red).bold
   end
 
   def computer_code_created
-    "The computer has chosen its 4-digit code."
+    'The computer has chosen its 4-digit code.'
   end
 
   def user_guess_prompt
-    "Please enter your guess:"
+    'Please enter your guess: '
   end
 
   def invalid_guess_warning
-    "Your guess should be a 4-digit number (each digit should be between 1-6):".colorize(:red).bold
+    'Your guess should be a 4-digit number (each digit should be between 1-6): '.colorize(:red).bold
   end
   
+  def display_turn
+    puts turn_number_display
+    guess_display
+    print clue_text
+    clues_display(clues)
+  end
+
   def turn_number_display
     "\nTurn: #{board.turn_number}\n\n"
   end
 
   def guess_display
-    guess.each { |number| print Board::NUMBER_OPTIONS_DISPLAY[number] + ' ' }
+    guess.each { |number| print "#{Board::NUMBER_OPTIONS_DISPLAY[number]} " }
   end
 
   def clues_display(clues)
-    clues.each { |clue| print clue + ' ' } 
+    clues.each { |clue| print "#{clue} " } 
     puts "\n\n"
   end
   
   def clue_text
-    "Clues for this guess: "
+    'Clues for this guess: '
   end
 
   def user_code_breaker_win
@@ -119,24 +127,24 @@ module GameText
 
   def reveal_code
     puts "This is the code you were trying to crack: \n"
-    code.each { |number| print Board::NUMBER_OPTIONS_DISPLAY[number] + ' ' }
+    code.each { |number| print "#{Board::NUMBER_OPTIONS_DISPLAY[number]} " }
     puts "\n\n"
   end
 
   def computer_code_breaker_win
-    "Game over. The computer broke your code."
+    'Game over. The computer broke your code.'
   end
 
   def computer_code_breaker_loss
-    "Congratulations, you broke the computer. No but actually how did you get here?"
+    'Congratulations, you broke the computer. No but actually how did you get here?'
   end
 
   def prompt_replay
-    "Play again? Press Y for Yes, or anything else for no."
+    'Play again? Press Y for Yes, or anything else for no.'
   end
 end
 
-#Class - The Mastermind game 'Board'
+# Class - The Mastermind game 'Board'
 class Board
   attr_accessor :turn_number
 
@@ -151,23 +159,23 @@ class Board
   NUMBER_OPTIONS = Array(1..6)
 
   NUMBER_OPTIONS_DISPLAY = {
-    1 => "#{'  1  '.colorize(:light_white).on_red}",
-    2 => "#{'  2  '.colorize(:light_white).on_yellow}",
-    3 => "#{'  3  '.colorize(:light_white).on_green}",
-    4 => "#{'  4  '.colorize(:light_white).on_blue}",
-    5 => "#{'  5  '.colorize(:light_white).on_cyan}",
-    6 => "#{'  6  '.colorize(:light_white).on_magenta}"
-  }
+    1 => '  1  '.colorize(:light_white).on_red,
+    2 => '  2  '.colorize(:light_white).on_yellow,
+    3 => '  3  '.colorize(:light_white).on_green,
+    4 => '  4  '.colorize(:light_white).on_blue,
+    5 => '  5  '.colorize(:light_white).on_cyan,
+    6 => '  6  '.colorize(:light_white).on_magenta
+  }.freeze
 
   CLUE_OPTIONS_DISPLAY = {
-    correct_number_correct_position: "#{'●'.colorize(:red)}",
-    correct_number_wrong_position: "○"
-  } 
+    correct_number_correct_position: '●'.colorize(:red),
+    correct_number_wrong_position: '○'
+  }.freeze
 
   MAX_TURN_NUMBER = 12
 end
 
-# Class - Code-Breaking behavior for the computer 
+# Class - Code-Breaking behavior for the computer
 class ComputerCodeBreaker
 
   attr_accessor :guess, :remaining_possible_codes, :min_scores
@@ -181,9 +189,9 @@ class ComputerCodeBreaker
   end
   
   def get_guess(turn, previous_clue)
-    if turn == 1 
+    if turn == 1
       guess
-    else 
+    else
       @previous_clue = previous_clue
       eliminate_impossible_codes(remaining_possible_codes)
       self.guess = find_next_guess
@@ -194,8 +202,8 @@ class ComputerCodeBreaker
 
   def all_possible_codes(min, max, code_length)
     array = Array(min..max)
-    array.repeated_permutation(code_length){ |permutation| array.push(permutation) }
-    array = array - Array(min..max)
+    array.repeated_permutation(code_length) { |permutation| array.push(permutation) }
+    array -= Array(min..max)
   end
 
   def eliminate_impossible_codes(array)
@@ -307,11 +315,12 @@ class CodeMaker
   def get_user_code
     get_user_code_input
     user_code_to_int_array
-    unless user_code_valid?
+    if user_code_valid?
+      return user_input_array
+    else
       puts invalid_user_code_warning
       get_user_code
     end
-    user_input_array
   end
 
   def get_user_code_input
@@ -373,7 +382,6 @@ class Clue
       hash[index][:guess_value] = guess[index]
       hash[index][:code_value] = code[index]
       hash[index][:number_and_position_correct?] = number_and_position_true?(index)
-
       hash
     end
   end
@@ -409,35 +417,27 @@ class Clue
 
   def add_clues
     index_hash.each do |index_key, hash|
-      
       if hash[:number_and_position_correct?]
-        hash[:clue] = 'correct number correct position' #?
         hash[:clue_display] = Board::CLUE_OPTIONS_DISPLAY[:correct_number_correct_position]
       elsif hash[:number_correct?]
-        #
-        hash[:clue] = 'correct number wrong position' #?
         hash[:clue_display] = Board::CLUE_OPTIONS_DISPLAY[:correct_number_wrong_position]
       end
     end
   end
 
   def clue_array
-    index_hash.map do |index_key, hash_value|
-      hash_value[:clue_display]
-    end
+    index_hash.map { |index_key, hash_value| hash_value[:clue_display] }
   end
 
   def remove_nil_values
     clue_array.compact
   end
-
 end
 
-# Module containing functions to select the user's role
+# Module - Namespace for functions that handle user role selection 
 module ChooseGameRole
   def choose_role
     puts choose_role_prompt
-    
     @user_role = user_role_number_to_role
   end
 
@@ -445,11 +445,8 @@ module ChooseGameRole
     gets.chomp.to_i
   end
   
-  
-  
   def user_role_number_to_role
     user_role_number = user_role_number_input
-    
     if user_role_number_valid?(user_role_number)
       assign_role(user_role_number)
     else
@@ -467,6 +464,7 @@ module ChooseGameRole
   end
 end
 
+# Game Class - Contains the core game logic
 class Game
   include Rules
   include GameText
@@ -478,10 +476,7 @@ class Game
     @board = Board.new
     instructions
     choose_role
-    
     play_game
-
-    end_of_game_handling  
   end
 
   private
@@ -492,13 +487,12 @@ class Game
     puts computer_code_created if user_role == 'code breaker'
     initialize_code_breaker
     game_loop
+    end_of_game
   end
-
 
   def initialize_code_maker
     @code = CodeMaker.new.get_code(user_role)
   end
-
 
   def initialize_code_breaker
     @code_breaker = user_role == 'code maker' ? ComputerCodeBreaker.new : UserCodeBreaker.new
@@ -506,16 +500,10 @@ class Game
 
   def game_loop
     loop do
-      puts user_guess_prompt if user_role == 'code breaker'
-      
+      puts user_guess_prompt if user_role == 'code breaker'  
       @guess = code_breaker.get_guess(board.turn_number, clues)
       @clues = Clue.new(guess, code).return_clues
-      
-      puts turn_number_display
-      guess_display
-      print clue_text
-      clues_display(clues)
-      
+      display_turn
       board.increment_turn
       break if game_over?
     end
@@ -533,13 +521,11 @@ class Game
     clues.all? do |clue| 
       clue == Board::CLUE_OPTIONS_DISPLAY[:correct_number_correct_position]
     end && clues.length == 4
-    
   end
 
-  def end_of_game_handling
+  def end_of_game
     end_of_game_message
     puts prompt_replay
-
     if replay?
       replay_game
     else
@@ -562,9 +548,6 @@ class Game
         puts computer_code_breaker_loss
       end
     end
-    #elsif user_role = code maker
-      #if code_breaker_wins?
-        #computer_code_breaker_win
   end
 
   def replay?
